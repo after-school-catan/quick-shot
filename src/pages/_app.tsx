7af281846a863a28c5179b0490640f96378e2ecf
miratetakonitoha <miratetakonitoha@gmail.com>
2025-05-04 15:39:15 +0900

    lintの設定を追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,5 +2,6 @@ import type { AppProps } from 'next/app';
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const hoge = 2;
   return <Component {...pageProps} />;
 }
